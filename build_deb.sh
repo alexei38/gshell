2a6660585ed00fd65896f8bca434892ff9a90215
Alexei Margasov <amargasov@naumen.ru>
2016-07-13 19:15:10 +0500

    fix build

--- build_deb.sh
+++ build_deb.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 
+export DEBEMAIL='alexei@margasov.ru'
+export DEBFULLNAME='Alexei Margasov'
+
 src_dir=$(dirname $0)
 src_dir=$(readlink -f $src_dir)
 
-if type docker >/dev/null 2>&1 ; then
-    docker pull debian:7
-    docker run --name gshell_build -tid --privileged -v $src_dir:/src debian:7
-    docker exec -t gshell_build apt-get update
-    docker exec -t gshell_build apt-get install -y dpkg debconf debhelper md5deep git
-    docker exec -t gshell_build /bin/sh -c 'mkdir -p /build/gshell/usr/share/applications; \
-                                            mkdir -p /build/gshell/usr/share/icons/; \
-                                            mkdir -p /build/gshell/usr/bin/; \
-                                            cp -r /src/DEBIAN /build/gshell/; \
-                                            cp /src/gshell /build/gshell/usr/bin/; \
-                                            cp /src/Gshell.desktop /build/gshell/usr/share/applications/; \
-                                            cp /src/src/icon/gshell.png /build/gshell/usr/share/icons/; \
-                                            cp -r /src/src /build/gshell/usr/share/gshell; \
-                                            cd /build/gshell; \
-                                            version=$(cd /src/; git describe); \
-                                            sed -i "s/VERSION/$version/g" DEBIAN/control; \
-                                            md5deep -r usr > DEBIAN/md5sums; \
-                                            cd /build; \
-                                            fakeroot dpkg-deb --build gshell /src/'
+cd $src_dir
+rm -rf build
+mkdir -p build
+cp -r debian build/debian
+cp -r src build/src
+cp gshell build/gshell
+cp Gshell.desktop build/
+cd build
 
-    docker stop gshell_build
-    docker rm gshell_build
-    echo 'DEB package locate in' $src_dir
-    find $src_dir -iname '*.deb'
-else
-    echo 'Need docker for build'
-    exit 1
-fi
+version=$(git describe | grep -E -o "^[[:digit:]]{1,}\.[[:digit:]]{1,}\.[[:digit:]]{1,}-[[:digit:]]{1,}")
+debchange --create --package gshell -v $version -D trusty --empty "Some bugs fixed"
+dpkg-buildpackage -B -rfakeroot
+#dput ppa:alexei38/gshell gshell\_$version\_source.changes
--- build_src.sh
+++ build_src.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+export DEBEMAIL='alexei@margasov.ru'
+export DEBFULLNAME='Alexei Margasov'
+
 src_dir=$(dirname $0)
 src_dir=$(readlink -f $src_dir)
 
 cd $src_dir
-version=$(git describe)
-rm -f debian/changelog
+rm -rf build
+mkdir -p build
+cp -r debian build/debian
+cp -r src build/src
+cp gshell build/gshell
+cp Gshell.desktop build/
+cd build
+
+version=$(git describe | grep -E -o "^[[:digit:]]{1,}\.[[:digit:]]{1,}\.[[:digit:]]{1,}-[[:digit:]]{1,}")
 debchange --create --package gshell -v $version -D trusty --empty "Some bugs fixed"
 dpkg-buildpackage -S -rfakeroot
-
-#dput ppa:alexei38/gshell <source.changes>
+#dput ppa:alexei38/gshell gshell\_$version\_source.changes
