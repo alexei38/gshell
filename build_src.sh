cdbd5b096bfeb575ac3997112c549be55c030ef8
Alexei Margasov <amargasov@naumen.ru>
2017-11-23 14:34:58 +0500

    fix versions

--- build_src.sh
+++ build_src.sh
@@ -7,6 +7,8 @@ src_dir=$(dirname $0)
 src_dir=$(readlink -f $src_dir)
 
 cd $src_dir
+
+
 rm -rf build
 mkdir -p build
 cp -r debian build/debian
@@ -15,11 +17,10 @@ cp gshell build/gshell
 cp Gshell.desktop build/
 find build -iname '*.pyc' -delete
 version=$(git describe | grep -E -o "^[[:digit:]]{1,}\.[[:digit:]]{1,}\.[[:digit:]]{1,}-[[:digit:]]{1,}")
+    sed -i "s/^VERSION.*/VERSION = '$version'/" build/src/about.py
 
-sed -i "s/^VERSION.*/VERSION = '$version'/" src/about.py
 cd build
 debchange --create --package gshell -v $version -D xenial --empty "Some bugs fixed"
 dpkg-buildpackage -S -rfakeroot
 cd ..
-git checkout -- src/about.py
-dput ppa:alexei38/gshell gshell_$version\_source.changes
+#dput ppa:alexei38/gshell gshell_$version\_source.changes
